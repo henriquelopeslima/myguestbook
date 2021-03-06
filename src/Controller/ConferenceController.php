48355214eccb6f01532e3e1b2f46118818a2d5d1
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2021-03-06 19:46:51 -0300

    building user interface

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,28 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
-  /**
-   * @Route("/hello/{name}", name="homepage")
-   */
-  public function index(string $name = ''): Response
-  {
-    $greet = '';
-    if ($name) {
-      $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
     }
-    return new Response(<<<EOF
-    <html>
-        <body>
-            $greet
-            <img src="/images/construction.gif" />
-        </body>
-    </html>
-    EOF);
-  }
+
+    /**
+     * @Route("/", name="homepage")
+     * @param ConferenceRepository $conferenceRepository
+     * @return Exception|Response|LoaderError|RuntimeError|SyntaxError
+     */
+    public function index(ConferenceRepository $conferenceRepository)
+    {
+        try {
+            return new Response($this->twig->render('conference/index.html.twig', [
+                'conferences' => $conferenceRepository->findAll(),
+            ]));
+        } catch (LoaderError | RuntimeError | SyntaxError $e) {
+            return $e;
+        }
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     * @param Request $request
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @return Exception|Response|LoaderError|RuntimeError|SyntaxError
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        try {
+            return new Response($this->twig->render('conference/show.html.twig', [
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            ]));
+        } catch (LoaderError | SyntaxError | RuntimeError $e) {
+            return $e;
+        }
+    }
 }
